d17cbfcf90586465af51fd9a8bdbf0a945f114e8
Vishal Mali <malivishal9826@gmail.com>
2022-12-31 20:37:02 +0530

    Minor Change in src/index

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require('./config/serverConfig');
 
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 const setupAndStartServer = async() => {
     // create the express object
     const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+
     app.listen(PORT, () => {
         console.log(`Server started at ${PORT}`);
     });
